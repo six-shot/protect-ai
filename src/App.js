401e769e8109cf0f2cedf9934e49c3f7b5f053b0
six-shot <okhuomonajayi54@gmail.com>
2024-12-13 17:13:39 +0100

    fix

--- src/App.js
+++ src/App.js
@@ -4,14 +4,17 @@ import Navbar from "./components/navbar";
 import Hero from "./modules/sections/hero";
 import Features from "./modules/sections/features";
 import Scam from "./modules/sections/scam";
+import Portfolio from "./modules/sections/portfolio";
 
 function App() {
+  
   return (
     <div className="w-full ">
       <Navbar />
       <Hero/>
       <Features/>
       <Scam/>
+      <Portfolio/>
     </div>
   );
 }
---
+++ src/modules/sections/portfolio/index.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import video4 from "../../../assets/video4.mp4"
+
+export default function Portfolio() {
+  return (
+    <div>
+      <video src={video4} autoPlay loop muted playsInline />
+    </div>
+  );
+}
