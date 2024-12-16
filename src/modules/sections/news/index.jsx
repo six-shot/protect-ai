77aac0728a792bbc899d7bda4646eb0a2af6aa9e
six-shot <okhuomonajayi54@gmail.com>
2024-12-16 22:34:55 +0100

    fix

--- src/modules/sections/news/index.jsx
+++ src/modules/sections/news/index.jsx
@@ -1,13 +1,97 @@
-import React from 'react'
-
+import React from "react";
+import { FaArrowRight } from "react-icons/fa6";
 export default function News() {
   return (
-    <div>
-      {/* <div className="py-20 bg-white reyhan-medium leading-[1.1em] tracking-[-1px]">
-        <h4 className="text-[clamp(24px,_15.069px_+_100vw*_.0229,_54px)] mb-7">
-          Clear Street in the News
-        </h4>
-      </div> */}
+    <div className="px-[350px] py-[300px] bg-white">
+      <div className="max-w-[1440px] mx-auto">
+        <div className="reyhan-medium tracking-[-1px]">
+          <h4 className="text-[clamp(24px,_15.069px_+_100vw*_.0229,_54px)]">
+            Clear Street in the News
+          </h4>
+        </div>
+        <div className="flex flex-col gap-6 reyhan-regular mt-20">
+          <div className="relative flex items-center h-full group cursor-pointer">
+            <div className="bg-[#f1f4f9] w-full px-[22px] py-[26px] flex gap-10 items-start rounded-[16px] relative z-10">
+              <h6 className="text-[#01001f]/40 text-sm">Dec 4, 2024</h6>
+              <div className="flex items-center gap-[5px]">
+                <span className="text-[#01001f]/40 text-[22px] leading-[1.1em]">
+                  Bloomberg
+                </span>
+                <h6 className="text-[#01001f] text-[22px] leading-[1.1em]">
+                  Fintech Firm Clear Streets Joins LMEs as Floor-Dealing Broker
+                </h6>
+              </div>
+            </div>
+            <div className="absolute top-0 right-0 w-[100px] flex items-center justify-center h-full  bg-[#2e21de] transform translate-x-[-20px] transition-transform duration-200 group-hover:translate-x-[80px] rounded-r-[16px]">
+              <FaArrowRight className="text-white -rotate-[45deg] text-[20px]" />
+            </div>
+          </div>
+          <div className="relative flex items-center h-full group cursor-pointer">
+            <div className="bg-[#f1f4f9] w-full px-[22px] py-[26px] flex gap-10 items-start rounded-[16px] relative z-10">
+              <h6 className="text-[#01001f]/40 text-sm">Dec 4, 2024</h6>
+              <div className="flex items-center gap-[5px]">
+                <span className="text-[#01001f]/40 text-[22px] leading-[1.1em]">
+                  Bloomberg
+                </span>
+                <h6 className="text-[#01001f] text-[22px] leading-[1.1em]">
+                  Fintech Firm Clear Streets Joins LMEs as Floor-Dealing Broker
+                </h6>
+              </div>
+            </div>
+            <div className="absolute top-0 right-0 w-[100px] flex items-center justify-center h-full  bg-[#2e21de] transform translate-x-[-20px] transition-transform duration-200 group-hover:translate-x-[80px] rounded-r-[16px]">
+              <FaArrowRight className="text-white -rotate-[45deg] text-[20px]" />
+            </div>
+          </div>
+          <div className="relative flex items-center h-full group cursor-pointer">
+            <div className="bg-[#f1f4f9] w-full px-[22px] py-[26px] flex gap-10 items-start rounded-[16px] relative z-10">
+              <h6 className="text-[#01001f]/40 text-sm">Dec 4, 2024</h6>
+              <div className="flex items-center gap-[5px]">
+                <span className="text-[#01001f]/40 text-[22px] leading-[1.1em]">
+                  Bloomberg
+                </span>
+                <h6 className="text-[#01001f] text-[22px] leading-[1.1em]">
+                  Fintech Firm Clear Streets Joins LMEs as Floor-Dealing Broker
+                </h6>
+              </div>
+            </div>
+            <div className="absolute top-0 right-0 w-[100px] flex items-center justify-center h-full  bg-[#2e21de] transform translate-x-[-20px] transition-transform duration-200 group-hover:translate-x-[80px] rounded-r-[16px]">
+              <FaArrowRight className="text-white -rotate-[45deg] text-[20px]" />
+            </div>
+          </div>
+          <div className="relative flex items-center h-full group cursor-pointer">
+            <div className="bg-[#f1f4f9] w-full px-[22px] py-[26px] flex gap-10 items-start rounded-[16px] relative z-10">
+              <h6 className="text-[#01001f]/40 text-sm">Dec 4, 2024</h6>
+              <div className="flex items-center gap-[5px]">
+                <span className="text-[#01001f]/40 text-[22px] leading-[1.1em]">
+                  Bloomberg
+                </span>
+                <h6 className="text-[#01001f] text-[22px] leading-[1.1em]">
+                  Fintech Firm Clear Streets Joins LMEs as Floor-Dealing Broker
+                </h6>
+              </div>
+            </div>
+            <div className="absolute top-0 right-0 w-[100px] flex items-center justify-center h-full  bg-[#2e21de] transform translate-x-[-20px] transition-transform duration-200 group-hover:translate-x-[80px] rounded-r-[16px]">
+              <FaArrowRight className="text-white -rotate-[45deg] text-[20px]" />
+            </div>
+          </div>
+          <div className="relative flex items-center h-full group cursor-pointer">
+            <div className="bg-[#f1f4f9] w-full px-[22px] py-[26px] flex gap-10 items-start rounded-[16px] relative z-10">
+              <h6 className="text-[#01001f]/40 text-sm">Dec 4, 2024</h6>
+              <div className="flex items-center gap-[5px]">
+                <span className="text-[#01001f]/40 text-[22px] leading-[1.1em]">
+                  Bloomberg
+                </span>
+                <h6 className="text-[#01001f] text-[22px] leading-[1.1em]">
+                  Fintech Firm Clear Streets Joins LMEs as Floor-Dealing Broker
+                </h6>
+              </div>
+            </div>
+            <div className="absolute top-0 right-0 w-[100px] flex items-center justify-center h-full  bg-[#2e21de] transform translate-x-[-20px] transition-transform duration-200 group-hover:translate-x-[80px] rounded-r-[16px]">
+              <FaArrowRight className="text-white -rotate-[45deg] text-[20px]" />
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
