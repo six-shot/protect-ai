b6bee5fa46a6645fa485ef92e4afbc8a6b20abe5
six-shot <okhuomonajayi54@gmail.com>
2024-12-18 13:46:23 +0100

    fix

--- src/components/navbar/index.jsx
+++ src/components/navbar/index.jsx
@@ -1,25 +1,25 @@
 import React from "react";
 import logo from "../../assets/logo.svg";
 import caretdown from "../../assets/icons/caret-down.svg";
+import { RiTwitterXLine } from "react-icons/ri";
+import { LiaTelegramPlane } from "react-icons/lia";
 import search from "../../assets/icons/search.svg";
 export default function Navbar() {
   return (
     <div className="fixed z-[20] w-full top-[35px]">
       <div className="flex justify-center items-center reyhan-regular px-[10%]">
         <div className="h-[52px] bg-[#f2f4fdd1] backdrop-blur-[7.5px] flex md:justify-normal justify-between w-full md:w-auto md:gap-[max(26.4px,_calc(26.4px_+_100vw_*_0.0339))] rounded-[max(23.5px,_calc(23.5px_+_100vw_*_0.0185))] items-center  pl-6 pr-2.5 shadow-[0_1px_0_0_#e5efff]">
-          <img src={logo} className="w-[100px]" alt="logo" />
+          <h4 className="text-[#2e21de]">LOGO</h4>
           <nav className="md:flex hidden ">
             <ul className="flex gap-[min(4px,_4px_+_100vw_*_0)] items-center text-[#2e21de] text-sm ">
-              <li className="flex gap-[2px] items-center pl-2 pr-2.5">
-                <button>Company</button>
-                <img
-                  src={caretdown}
-                  className="w-[16px] h-[16px] mt-1"
-                  alt="caretdown"
-                />
+              <li className="flex gap-[2px] items-center px-2.5">
+                <button>Home</button>
+              </li>
+              <li className="flex gap-[2px] items-center px-2.5">
+                <button>About</button>
               </li>
               <li className="flex gap-[2px] items-center pl-2 pr-2.5">
-                <button>Services</button>
+                <button>Feature</button>
                 <img
                   src={caretdown}
                   className="w-[16px] h-[16px] mt-1"
@@ -27,22 +27,18 @@ export default function Navbar() {
                 />
               </li>
               <li className="flex gap-[2px] items-center px-2.5">
-                <button>Studio</button>
-              </li>
-              <li className="flex gap-[2px] items-center px-2.5">
-                <button>Careers</button>
+                <button>Buy</button>
               </li>
             </ul>
           </nav>
           <div className="md:flex hidden  gap-2.5 items-center text-sm">
-            <div className="w-[32px] h-[32px] flex justify-center items-center">
-              <img src={search} alt="search" />
-            </div>
+            <RiTwitterXLine className="text-[21px] text-[#2e21de] cursor-pointer" />
+            <LiaTelegramPlane className="text-[22px] text-[#2e21de] cursor-pointer" />
             <button className="h-[32px] px-3 border text-[#2e21de] border-[#2e21de]/20 rounded-[max(23.5px,_calc(23.5px_+_100vw_*_0.0185))]">
-              Login
+              Chart
             </button>
             <button className="h-[32px] px-3  text-white bg-[#2e21de] rounded-[max(23.5px,_calc(23.5px_+_100vw_*_0.0185))]">
-              Get in touch
+              Enter DApp
             </button>
           </div>
           <div className="md:hidden flex">
