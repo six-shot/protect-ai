c6646f3f4fbbb2c2f32b6ab4499b35d130b7ee1d
six-shot <okhuomonajayi54@gmail.com>
2024-12-18 17:10:44 +0100

    fix

--- src/modules/sections/portfolio/index.jsx
+++ src/modules/sections/portfolio/index.jsx
@@ -9,7 +9,7 @@ export default function Portfolio() {
         <div className="absolute top-10 w-full   text-white z-[10] ">
           <div className="flex flex-col justify-center items-center reyhan-medium">
             <h2 className="md:text-[56px] text-[28px]  md:w-[848px] leading-[1.1em] tracking-[-2px] text-center mb-[36px]">
-              The All In One AI tool to keep you Secured from Scam Threat
+              Cutting-edge Cybersecurity Platform Designed to keep you Secured.
             </h2>
             <button className="h-[40px] px-3  text-white bg-[#2e21de] rounded-[max(23.5px,_calc(23.5px_+_100vw_*_0.0185))]">
               Request a demo
@@ -22,14 +22,14 @@ export default function Portfolio() {
               <h2 className="text-[46px] text-[#656578]   leading-[1.1em] tracking-[-2px] mb-4">
                 <span className="text-white">
                   Cutting-edge cybersecurity
-                  <br /> platform  to safeguard users
+                  <br /> platform to safeguard users
                   <br />
                 </span>{" "}
                 to drive alpha and power
                 <br /> decision-making.
               </h2>
               <button className="mt-5 h-[40px] px-3  text-white bg-[#2e21de] rounded-[max(23.5px,_calc(23.5px_+_100vw_*_0.0185))]">
-                View Studio
+                Try Now
               </button>
             </div>
           </div>
